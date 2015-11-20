f42533190595b5399a1dad9de7d6307a8dd26135
applejackman <jackson@norphar.com>
2015-11-20 10:35:15 -0600

    Now working for our config

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,12 +1,14 @@
-#define LEFT_PIN 1
-
+#define LEFT_PIN 4
+#define RIGHT_PIN 6
 int lastLeft = LOW;
+int lastRight = LOW;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Controller has started");
   pinMode(LEFT_PIN, INPUT);
+  pinMode(RIGHT_PIN, INPUT);
 }
 
 void loop() {
@@ -16,5 +18,11 @@ void loop() {
       Serial.print("Left pin: ");
       Serial.println(currentLeft);
       lastLeft = currentLeft;
+  }
+       int currentRight = digitalRead(RIGHT_PIN);
+       if (currentRight != lastRight) {
+      Serial.print("Right pin: ");
+      Serial.println(currentRight);
+      lastRight = currentRight;
   }
 }
